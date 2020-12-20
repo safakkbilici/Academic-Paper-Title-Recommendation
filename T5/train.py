4b62e0c49650533bd222d44303510e1b02689cab
Şafak Bilici <safakk.bilici.2112@gmail.com>
2020-12-21 00:28:04 +0300

    train.py [colab is recommended, and scripts are provided for colab]

--- T5/train.py
+++ T5/train.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from simpletransformers.t5 import T5Model
+from google.colab import drive
+import pandas as pd
+drive.mount('/content/drive',force_remount=True)
 
-df = pd.read_csv('../data/df_to_model.csv')
+
+df = pd.read_csv('/content/drive/My Drive//data/df_to_model.csv')
 eval_df = df.sample(frac=0.2, random_state=101)
 train_df = df.drop(eval_df.index)
 
@@ -22,12 +26,11 @@ model_args = {
     "evaluate_during_training_steps": 15000,
     "evaluate_during_training_verbose": True,
     "fp16": False,
-
-    "wandb_project": "Summarization with T5",
+    "best_model_dir": "/content/drive/My Drive/outputs/best_model",
+    "output_dir": "/content/drive/My Drive/outputs",
+    "wandb_project": "Paper Summarization with T5",
 }
 
 model = T5Model("t5", "t5-base", args=model_args)
-
 model.train_model(train_df, eval_data=eval_df)
-
 
