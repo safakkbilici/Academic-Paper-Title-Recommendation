f1e1b4bd04860db4305a9545095d715bb90ccc35
safakkbilici <safakk.bilici@gmail.com>
2021-01-06 21:24:22 +0300

    fit text added

--- train_lstm.py
+++ train_lstm.py
@@ -6,6 +6,74 @@ from lstm_seq2seq.library.utility.plot_utils import plot_and_save_history
 from lstm_seq2seq.library.seq2seq import Seq2SeqSummarizer
 import numpy as np
 
+from collections import Counter
+
+MAX_INPUT_SEQ_LENGTH = 500
+MAX_TARGET_SEQ_LENGTH = 50
+MAX_INPUT_VOCAB_SIZE = 5000
+MAX_TARGET_VOCAB_SIZE = 2000
+
+
+def fit_text(X, Y, input_seq_max_length=None, target_seq_max_length=None):
+    if input_seq_max_length is None:
+        input_seq_max_length = MAX_INPUT_SEQ_LENGTH
+    if target_seq_max_length is None:
+        target_seq_max_length = MAX_TARGET_SEQ_LENGTH
+    input_counter = Counter()
+    target_counter = Counter()
+    max_input_seq_length = 0
+    max_target_seq_length = 0
+
+    for line in X:
+        text = [word.lower() for word in line.split(' ')]
+        seq_length = len(text)
+        if seq_length > input_seq_max_length:
+            text = text[0:input_seq_max_length]
+            seq_length = len(text)
+        for word in text:
+            input_counter[word] += 1
+        max_input_seq_length = max(max_input_seq_length, seq_length)
+
+    for line in Y:
+        line2 = 'START ' + line.lower() + ' END'
+        text = [word for word in line2.split(' ')]
+        seq_length = len(text)
+        if seq_length > target_seq_max_length:
+            text = text[0:target_seq_max_length]
+            seq_length = len(text)
+        for word in text:
+            target_counter[word] += 1
+            max_target_seq_length = max(max_target_seq_length, seq_length)
+
+    input_word2idx = dict()
+    for idx, word in enumerate(input_counter.most_common(MAX_INPUT_VOCAB_SIZE)):
+        input_word2idx[word[0]] = idx + 2
+    input_word2idx['PAD'] = 0
+    input_word2idx['UNK'] = 1
+    input_idx2word = dict([(idx, word) for word, idx in input_word2idx.items()])
+
+    target_word2idx = dict()
+    for idx, word in enumerate(target_counter.most_common(MAX_TARGET_VOCAB_SIZE)):
+        target_word2idx[word[0]] = idx + 1
+    target_word2idx['UNK'] = 0
+
+    target_idx2word = dict([(idx, word) for word, idx in target_word2idx.items()])
+    
+    num_input_tokens = len(input_word2idx)
+    num_target_tokens = len(target_word2idx)
+
+    config = dict()
+    config['input_word2idx'] = input_word2idx
+    config['input_idx2word'] = input_idx2word
+    config['target_word2idx'] = target_word2idx
+    config['target_idx2word'] = target_idx2word
+    config['num_input_tokens'] = num_input_tokens
+    config['num_target_tokens'] = num_target_tokens
+    config['max_input_seq_length'] = max_input_seq_length
+    config['max_target_seq_length'] = max_target_seq_length
+
+    return config
+
 LOAD_EXISTING_WEIGHTS = True
 np.random.seed(170110)
 report_dir_path = './reports'
